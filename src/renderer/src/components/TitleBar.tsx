0aae1c5513941bacaf8bfbc9dc715e01d8036ee8
Tomonobu Terakubo <129341151+2b3d4f@users.noreply.github.com>
2025-03-15 15:15:12 +0900

    feat: update title text to 'Electron App' in TitleBar component

--- src/renderer/src/components/TitleBar.tsx
+++ src/renderer/src/components/TitleBar.tsx
@@ -4,7 +4,7 @@ function TitleBar(): JSX.Element {
   return (
     <TitleBarBase>
       <div className="bg-opacity-0 hover:bg-opacity-50 flex h-full items-center justify-between px-4 *:max-h-full">
-        <div className="text-[12px]">Title</div>
+        <div className="text-[12px]">Electron App</div>
       </div>
     </TitleBarBase>
   )
